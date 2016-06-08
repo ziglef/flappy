cc9983997347dd614cb1135a183e4a7169de7eb0
Rui Grandao <ruigrandaorocha@gmail.com>
2016-06-08 10:18:39 +0100

    Initial Commit

---
+++ desktop/src/io/ziglef/flappy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.ziglef.flappy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.ziglef.flappy.Flappy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Flappy(), config);
+	}
+}
