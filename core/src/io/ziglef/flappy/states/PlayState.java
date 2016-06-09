108873e7668630fd85d7875d785de14c0b0ffe4c
Rui Grandao <ruigrandaorocha@gmail.com>
2016-06-09 18:57:32 +0100

    Added scores!

--- core/src/io/ziglef/flappy/states/PlayState.java
+++ core/src/io/ziglef/flappy/states/PlayState.java
@@ -1,9 +1,10 @@
 package io.ziglef.flappy.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
@@ -22,6 +23,9 @@ public class PlayState extends State {
     private Texture ground;
     private Vector2 groundPos1, groundPos2;
     private Array<Tube> tubes;
+    private BitmapFont scoreFnt;
+    private int score;
+    private boolean score_hack;
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
@@ -32,10 +36,13 @@ public class PlayState extends State {
         groundPos1 = new Vector2(cam.position.x - cam.viewportWidth / 2, GROUND_Y_OFFSET);
         groundPos2 = new Vector2((cam.position.x - cam.viewportWidth / 2) + ground.getWidth(), GROUND_Y_OFFSET);
 
-
         for (int i = 1; i <= TUBE_COUNT; i++) {
             tubes.add(new Tube(i * (TUBE_SPACING + Tube.WIDTH)));
         }
+
+        scoreFnt = new BitmapFont();
+        score = 0;
+        score_hack = false;
 
         cam.setToOrtho(false, Flappy.WIDTH / 2, Flappy.HEIGHT / 2);
     }
@@ -53,8 +60,15 @@ public class PlayState extends State {
         updateGround();
 
         for(Tube tube : tubes){
-            if(cam.position.x - (cam.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth())
+            if(cam.position.x - (cam.viewportWidth / 2) > tube.getPosTopTube().x + tube.getTopTube().getWidth()) {
                 tube.reposition(tube.getPosTopTube().x + ((Tube.WIDTH + TUBE_SPACING) * TUBE_COUNT));
+                score_hack = false;
+            }
+
+            if(bird.getPosition().x > tube.getPosTopTube().x + tube.getTopTube().getWidth() && !score_hack) {
+                score++;
+                score_hack = true;
+            }
 
             if(tube.collides(bird.getBounds())) {
                 gsm.set(new PlayState(gsm));
@@ -85,6 +99,10 @@ public class PlayState extends State {
 
         sb.draw(ground, groundPos1.x, groundPos1.y);
         sb.draw(ground, groundPos2.x, groundPos2.y);
+
+        scoreFnt.setUseIntegerPositions(false);
+        scoreFnt.setColor(Color.BLACK);
+        scoreFnt.draw(sb, String.valueOf(score), cam.position.x + cam.viewportWidth / 2 - 25, cam.position.y + cam.viewportHeight / 2 - 25);
 
         sb.end();
 
